6db021638eab6ae8075e18e4ab0cfaabb1d5d930
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2024-08-11 20:11:54 -0300

    format menu

--- resources/views/components/partials/menu.blade.php
+++ resources/views/components/partials/menu.blade.php
@@ -1,7 +1,7 @@
 @props([
     'menuItems' => [],
     'class' => 'bg-white text-gray-700 hover:text-green-800 hover:font-bold',
-    'activeClass' => 'bg-green-200 text-green-900',
+    'activeClass' => 'bg-green-200 text-green-900 font-bold',
 ])
 
 @php
@@ -12,6 +12,11 @@
     function generateItemClass($item, $baseClass, $class, $activeClass) {
         return request()->is($item['route']) ? "$baseClass $activeClass" : "$baseClass $class";
     }
+
+    // Função para garantir que a rota comece com '/'
+    function formatRoute($route) {
+        return '/' . ltrim($route, '/');
+    }
 @endphp
 
 <div {{ $attributes->merge(['class' => 'w-56 bg-white h-screen shadow-lg']) }}>
@@ -21,7 +26,7 @@
     <nav class="text-base pt-3">
         @foreach ($menuItems as $menuItem)
             <div>
-                <a href="{{ $menuItem['route'] ?? '#' }}" 
+                <a href="{{ formatRoute($menuItem['route'] ?? '#') }}" 
                    class="{{ generateItemClass($menuItem, $baseClass, $class, $activeClass) }}">
                     <i class="{{ $menuItem['icon'] }} mr-3"></i>
                     <span>{{ $menuItem['name'] }}</span>
@@ -32,7 +37,7 @@
                 @if (isset($menuItem['subMenu']))
                     <div class="pl-6">
                         @foreach ($menuItem['subMenu'] as $subItem)
-                            <a href="{{ $subItem['route'] }}" 
+                            <a href="{{ formatRoute($subItem['route']) }}" 
                                class="{{ generateItemClass($subItem, $baseClass, $class, $activeClass) }}">
                                 <i class="{{ $subItem['icon'] }} mr-3"></i>
                                 <span>{{ $subItem['name'] }}</span>
