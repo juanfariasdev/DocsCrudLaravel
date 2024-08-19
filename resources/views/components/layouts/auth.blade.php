e1792d43d6501c868ca84d376364ea9b1af30e51
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2024-08-18 23:49:02 -0300

    updated to nps

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -1,8 +1,8 @@
 @extends('components.layouts.app')
 
 @section('content')
-<div class="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-    <div class="flex flex-col justify-center lg:w-2/3 xl:w-6/12 p-6 sm:p-12 min-h-[700px] h-auto">
+<div class="max-w-screen-2xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+    <div class="flex flex-col justify-center w-full lg:w-2/3 xl:w-6/12 p-6 sm:p-12 min-h-[700px] h-auto">
         <div class="w-full flex justify-center">
             <img src="{{ asset('images/logo.png') }}" alt="Logo" class="max-w-[300px] h-auto w-auto" />
         </div>
@@ -12,9 +12,9 @@
             </div>
         </div>
     </div>
-    <div class="flex-1 bg-primary text-center hidden lg:flex">
+    <div class="flex-1 bg-primary text-center hidden lg:flex bg-cover bg-no-repeat bg-right" style="background-image: url({{ asset('images/background.png') }});">
         <div class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
-             style="background-image: url({{ asset('images/code.png') }});">
+             >
         </div>
     </div>
 </div>
